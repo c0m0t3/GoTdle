86eab110099701fdb9c8a834a742b1120d70271d
c0m0t3 <colin.moissl@icloud.com>
2024-12-06 01:51:11 +0100

    feat. add AppRoutes.tsx

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,31 @@
+import { Navigate, Route, RouteProps, Routes } from "react-router-dom";
+import HomePage from './pages/HomePage.tsx';
+
+
+export type RouteConfig = RouteProps & {
+  /**
+   * Required route path. E.g. /home
+   */
+  path: string;
+  isPrivate?: boolean;
+};
+
+export const appRoutes: RouteConfig[] = [
+  {
+    path: '/',
+    element: <Navigate to="/home" replace />,
+    index: true
+  },
+  {
+    path: '/home',
+    element: <HomePage />
+  }
+];
+
+export function renderRouteMap({ element, ...restRoute }: RouteConfig) {
+  return <Route key={restRoute.path} {...restRoute} element={element} />;
+}
+
+export const AppRoutes = () => {
+  return <Routes>{appRoutes.map(renderRouteMap)}</Routes>;
+};
