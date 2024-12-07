57959c6666ad678be25dd36e5e0e73f8a286f375
c0m0t3 <colin.moissl@icloud.com>
2024-12-08 00:25:34 +0100

    ref. extract gotButtonStyle

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -12,6 +12,7 @@ import {
 } from '@chakra-ui/react';
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
+import { gotButtonStyle } from '../styles/buttonStyles.ts';
 
 export const HomePage = () => {
   const navigate = useNavigate();
@@ -132,23 +133,3 @@ export const HomePage = () => {
   );
 }
 
-const gotButtonStyle = {
-  background: 'rgb(120, 0, 0)',
-  color: 'white',
-  fontSize: '1.2em',
-  border: '2px solid #D2B48C',
-  borderRadius: '8px',
-  padding: '0.5em 1em',
-  textShadow: '1px 1px 2px #000',
-  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-  width: '15em',
-  marginBottom: '20px',
-  _hover: {
-    background: 'rgb(100, 0, 0)',
-    borderColor: '#DEB887',
-  },
-  _active: {
-    background: 'rgb(80, 0, 0)',
-    borderColor: '#8B4513',
-  },
-};
