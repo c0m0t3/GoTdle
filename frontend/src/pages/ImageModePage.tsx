0f1475004b202018a3abf5e5b42a6e29a27f72ac
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2024-12-22 16:21:15 +0100

    feat: modify ImageModePage and integrate the selectComponent

--- frontend/src/pages/ImageModePage.tsx
+++ frontend/src/pages/ImageModePage.tsx
@@ -1,38 +1,70 @@
 import { BaseLayout } from '../layout/BaseLayout.tsx';
-import { Box, Button, Image, Input, Text, VStack } from '@chakra-ui/react';
+import { Box, Button, Image, Text, VStack } from '@chakra-ui/react';
 import { useImageApi } from '../hooks/useImageApi.ts';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { CharacterSelect } from '../components/CharacterSelect.tsx';
+import { useApiClient } from '../hooks/useApiClient.ts';
+import { GroupBase } from 'react-select';
+import { OptionBase } from 'chakra-react-select';
+
+interface CharacterOption extends OptionBase {
+  label: string;
+  value: string;
+}
 
 export const ImageModePage = () => {
   const { fetchApi, apiData } = useImageApi();
-  const [inputValue, setInputValue] = useState('');
   const [incorrectGuesses, setIncorrectGuesses] = useState<string[]>([]);
   const [isCorrect, setIsCorrect] = useState(false);
+  const [selectedCharacter, setSelectedCharacter] = useState<CharacterOption | null>(null);
+  const [usedOptions, setUsedOptions] = useState<string[]>([]);
+
+  const client = useApiClient();
 
   useEffect(() => {
+    setIsCorrect(false);
+    setSelectedCharacter(null);
+    setIncorrectGuesses([]);
+    setUsedOptions([]);
     fetchApi().catch((error) => {
       console.error('Failed to fetch image:', error);
     });
   }, [fetchApi]);
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
+
+  const handleCharacterSelect = (selected: CharacterOption | null) => {
+    if (selected) {
+      setSelectedCharacter(selected);
+      if (selected.value.toLowerCase() === apiData?.fullName.toLowerCase()) {
+        setIsCorrect(true);
+      } else {
+        setIncorrectGuesses([selected.value, ...incorrectGuesses]);
+      }
+      setUsedOptions((prev) => [...prev, selected.value]);
+      setSelectedCharacter(null);
+    }
   };
 
-  const handleSubmit = () => {
-    if (inputValue.toLowerCase() === apiData?.fullName.toLowerCase()) {
-      setIsCorrect(true);
-    } else {
-      setIncorrectGuesses([inputValue, ...incorrectGuesses]);
+  const loadCharacterOptions = async (inputValue: string) => {
+    const characters = await client.getCharacters();
+    if (characters.status === 200) {
+      return characters.data
+        .filter((character) =>
+          character?.name?.toLowerCase().startsWith(inputValue.toLowerCase()) &&
+          !usedOptions.includes(character.name) // Filtere verwendete Optionen aus
+        )
+        .map((character) => ({
+          label: character.name ?? '',
+          value: character.name ?? ''
+        }));
     }
-    setInputValue('');
+    return [];
   };
 
   return (
     <BaseLayout>
       <Box p={4} display="flex" justifyContent="center" alignItems="center">
         <VStack>
-
 
           <Box
             position="relative"
@@ -49,17 +81,19 @@ export const ImageModePage = () => {
             <Text fontSize={'md'}>Which character is shown in this image?</Text>
             <Image src={apiData?.imageUrl} alt={apiData?.fullName} objectFit="contain" maxW="100%" maxH="100%" />
             <Text fontSize={'sm'}> Pssst...answer is...{apiData?.fullName}</Text>
-            <Input
-              mt={4}
-              placeholder="Type character name..."
-              value={inputValue}
-              onChange={handleInputChange}
-              isDisabled={isCorrect}
+
+            <CharacterSelect<CharacterOption, false, GroupBase<CharacterOption>>
+              name="character"
+              selectProps={{
+                isMulti: false,
+                placeholder: 'Type character name...',
+                loadOptions: loadCharacterOptions,
+                onChange: handleCharacterSelect,
+                value: selectedCharacter
+              }}
             />
-            <Button mt={2} onClick={handleSubmit} isDisabled={isCorrect}>Submit</Button>
-
-
           </Box>
+
           <Box maxW="sm" width="100%">
             {isCorrect && (
               <Text mt={2} textAlign={'center'} bg="green.500" color="white" p={2} rounded="md">
@@ -74,9 +108,11 @@ export const ImageModePage = () => {
               </Text>
             ))}
           </Box>
+
           {isCorrect && (
             <Button mt={4}>Next</Button>
           )}
+
         </VStack>
       </Box>
     </BaseLayout>
