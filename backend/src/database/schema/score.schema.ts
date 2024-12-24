473e2c31e0fd25f49a96fcb08ad4ef3da2431348
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2024-12-24 12:39:33 +0100

    fix: remove dafault value of lastPlayed and change dailyScore	o an array

--- backend/src/database/schema/score.schema.ts
+++ backend/src/database/schema/score.schema.ts
@@ -1,11 +1,20 @@
+import { sql } from 'drizzle-orm';
 import { integer, pgTable, timestamp, uuid } from 'drizzle-orm/pg-core';
 import { userSchema } from './user.schema';
 
-
 export const scoreSchema = pgTable('score', {
-    userId: uuid('userId').notNull().unique().references(() => userSchema.id, { onDelete: 'cascade' }),
-    streak: integer('streak').notNull().default(0),
-    lastPlayed: timestamp('lastPlayed').default(new Date()),
-    longestStreak: integer('longestStreak').default(0),
-    dailyScore: integer('dailyScore').default(0),
+  userId: uuid('userId')
+    .notNull()
+    .unique()
+    .references(() => userSchema.id, { onDelete: 'cascade' }),
+  streak: integer('streak').notNull().default(0),
+  lastPlayed: timestamp('lastPlayed'),
+  longestStreak: integer('longestStreak').notNull().default(0),
+  dailyScore: integer('dailyScore')
+    .notNull()
+    .default(
+      sql`ARRAY
+      [0]::INTEGER[]`,
+    )
+    .array(),
 });
