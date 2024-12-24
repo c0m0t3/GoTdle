c463ca0ddd7507fe97e9c09339be6efbdfcd33d8
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2024-12-25 00:07:47 +0100

    refactor: handle errors, remove redundant code, change sql style etc. in user.repository

--- backend/src/controller/user.controller.ts
+++ backend/src/controller/user.controller.ts
@@ -1,12 +1,16 @@
-import { Request, Response, NextFunction } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { UserRepository } from '../database/repository/user.repository';
 import { z } from 'zod';
-import { updateUserZodSchema, createUserZodSchema } from '../validation/validation';
+import { updateUserZodSchema } from '../validation/validation';
 
 export class UserController {
   constructor(private readonly userRepository: UserRepository) {}
 
-  async getUserById(req: Request, res: Response, next: NextFunction): Promise<void> {
+  async getUserById(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
     try {
       const idSchema = z.string().uuid();
       const userID = idSchema.parse(req.params.id);
@@ -23,7 +27,11 @@ export class UserController {
     }
   }
 
-  async getUserByUsername(req: Request, res: Response, next: NextFunction): Promise<void> {
+  async getUserByUsername(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
     try {
       const usernameSchema = z.string().min(3);
       const username = usernameSchema.parse(req.params.username);
@@ -40,7 +48,11 @@ export class UserController {
     }
   }
 
-  async getUserByEmail(req: Request, res: Response, next: NextFunction): Promise<void> {
+  async getUserByEmail(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
     try {
       const emailSchema = z.string().email();
       const email = emailSchema.parse(req.params.email);
@@ -57,52 +69,69 @@ export class UserController {
     }
   }
 
-
-
-async createUser(req: Request, res: Response, next: NextFunction): Promise<void> {
-  try {
-    const user = await this.userRepository.createUser(req.body);
-    res.status(201).json(user);
-  } catch (error) {
-    next(error);
-  }
-}
-
-async updateUser(req: Request, res: Response, next: NextFunction): Promise<void> {
-  try {
-    const userData = await updateUserZodSchema.parseAsync({ ...req.body, id: req.params.id });
-    const updatedUser = await this.userRepository.updateUser(userData);
-
-    if (!updatedUser) {
-      res.status(404).json({ errors: ['User not found'] });
-      return;
+  async createUser(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const user = await this.userRepository.createUser(req.body);
+      res.status(201).json(user);
+    } catch (error) {
+      next(error);
     }
-
-    res.status(200).json(updatedUser);
-  } catch (error) {
-    next(error);
   }
-}
 
-async deleteUser(req: Request, res: Response, next: NextFunction): Promise<void> {
-  try {
-    const idSchema = z.string().uuid();
-    const userId = idSchema.parse(req.params.id);
+  async updateUser(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const userData = await updateUserZodSchema.parseAsync({
+        ...req.body,
+        id: req.params.id,
+      });
+      const updatedUser = await this.userRepository.updateUserById(userData);
 
-    const deletedUser = await this.userRepository.deleteUser(userId);
+      if (!updatedUser) {
+        res.status(404).json({ errors: ['User not found'] });
+        return;
+      }
 
-    if (!deletedUser) {
-      res.status(404).json({ errors: ['User not found'] });
-      return;
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      next(error);
     }
-
-    res.status(204).send();
-  } catch (error) {
-    next(error);
   }
-}
 
-  async getAllUsers(req: Request, res: Response, next: NextFunction): Promise<void> {
+  async deleteUser(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
+    try {
+      const idSchema = z.string().uuid();
+      const userId = idSchema.parse(req.params.id);
+
+      const deletedUser = await this.userRepository.deleteUserById(userId);
+
+      if (!deletedUser) {
+        res.status(404).json({ errors: ['User not found'] });
+        return;
+      }
+
+      res.status(204).send();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getAllUsers(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ): Promise<void> {
     try {
       const users = await this.userRepository.getAllUsers();
       res.status(200).json(users);
