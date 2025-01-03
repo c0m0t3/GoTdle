84a77a644edef6525c33ccb9ece0d73e8ae1240a
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2025-01-03 17:56:17 +0100

    refactor: adjust AuthCard with BaseBox

--- frontend/src/components/AuthCard.tsx
+++ frontend/src/components/AuthCard.tsx
@@ -1,25 +1,16 @@
-import { Box, Center } from '@chakra-ui/react';
+import { Center } from '@chakra-ui/react';
+import { BaseBox } from './BaseBox.tsx';
 
 export const AuthCard = ({ children }: { children: React.ReactNode }) => {
   return (
     <Center h="full">
-      <Box
-        minH="400px"
-        w="400px"
-        // borderRadius="lg"
-        boxShadow="md"
-        p={4}
-        bgImage={"url('/bg_border.png')"}
-        // _dark={{ bg: 'gray.700' }}
-        bgSize="100% 100%"
-        bgRepeat="no-repeat"
-        bgPosition="top"
-        // borderRadius="md"
-        margin={10}
-        border="none"
+      <BaseBox
+        minH={'25em'}
+        w={'25em'}
+        m={10}
       >
         {children}
-      </Box>
+      </BaseBox>
     </Center>
   );
 };
