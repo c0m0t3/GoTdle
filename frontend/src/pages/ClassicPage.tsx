29c036836e63e646004905f423951ab4fbbd66f8
c0m0t3 <colin.moissl@icloud.com>
2025-01-03 19:12:45 +0100

    refactor: extract design

--- frontend/src/pages/ClassicPage.tsx
+++ frontend/src/pages/ClassicPage.tsx
@@ -1,17 +1,15 @@
 import { BaseLayout } from '../layout/BaseLayout.tsx';
-import { Box, Button, HStack, Text, VStack } from '@chakra-ui/react';
+import { Box, Text, VStack } from '@chakra-ui/react';
 import { CharacterGrid } from '../layout/CharacterGrid.tsx';
 import { useEffect, useState } from 'react';
-import { FaImage, FaQuestionCircle, FaQuoteRight } from 'react-icons/fa';
-import { gotButtonStyle } from '../styles/buttonStyles.ts';
 import { useApiClient } from '../hooks/useApiClient.ts';
 import { CharacterSelect } from '../components/CharacterSelect.tsx';
 import { GroupBase } from 'react-select';
 import { OptionBase } from 'chakra-react-select';
-import { useNavigate } from 'react-router-dom';
-import { CountdownTimer } from '../components/CountdownTimer.tsx';
-import { PulsingButton } from '../components/PulsingButton.tsx';
 import murmurhash from 'murmurhash';
+import { ModeNavigationBox } from '../components/ModeNavigationBox.tsx';
+import { BaseBox } from '../components/BaseBox.tsx';
+import { ModeSuccessBox } from '../components/ModeSuccessBox.tsx';
 
 interface Character {
   name: string;
@@ -36,7 +34,6 @@ export const ClassicPage: React.FC = () => {
   const [correctGuess, setCorrectGuess] = useState<string>('');
   const [usedOptions, setUsedOptions] = useState<string[]>([]);
   const client = useApiClient();
-  const navigate = useNavigate();
 
   const getCharacterOfTheDay = (characters: Character[]) => {
     const date = new Date();
@@ -110,18 +107,6 @@ export const ClassicPage: React.FC = () => {
     return [];
   };
 
-  const handleNavigateToQuote = () => {
-    navigate('/quote');
-  };
-
-  const handleNavigateToImage = () => {
-    navigate('/image');
-  };
-
-  const handleNavigateToClassic = () => {
-    navigate('/classic');
-  };
-
   return (
     <BaseLayout>
       <Box
@@ -135,48 +120,14 @@ export const ClassicPage: React.FC = () => {
         border="none"
       >
         <VStack>
-          <Box
-            bgImage={'url(\'/bg_border.png\')'}
-            bgSize="100% 100%"
-            bgRepeat="no-repeat"
-            bgPosition="top"
-            p={8}
-            borderRadius="md"
-            margin={4}
-          >
-            <HStack>
-              <Button style={gotButtonStyle} width={'8em'} leftIcon={<FaQuestionCircle />}
-                      onClick={handleNavigateToClassic}> Classic </Button>
-              <Button style={gotButtonStyle} width={'8em'} leftIcon={<FaQuoteRight />}
-                      onClick={handleNavigateToQuote}> Quote </Button>
-              <Button style={gotButtonStyle} width={'8em'} leftIcon={<FaImage />}
-                      onClick={handleNavigateToImage}> Image </Button>
-            </HStack>
-          </Box>
-          <Box
-            bgImage={'url(\'/bg_border.png\')'}
-            bgSize="100% 100%"
-            bgRepeat="no-repeat"
-            bgPosition="top"
-            p={4}
-            borderRadius="md"
-            margin={0}
-            width={'30em'}
-          >
+          <ModeNavigationBox />
+          <BaseBox>
             <Text textAlign={'center'}> Guess today's Game of Thrones character! </Text>
             <Text textAlign={'center'}> Type any character to begin. </Text>
             <Text textAlign={'center'}> DEBUG: The Solution is </Text>
             <Text textAlign={'center'}> {solutionCharacter?.name} </Text>
-          </Box>
-          <Box
-            bgImage={'url(\'/bg_border.png\')'}
-            bgSize="100% 100%"
-            bgRepeat="no-repeat"
-            bgPosition="top"
-            p={4}
-            borderRadius="md"
-            minWidth={'30em'}
-          >
+          </BaseBox>
+          <BaseBox>
             <CharacterSelect<CharacterOption, false, GroupBase<CharacterOption>>
               name="character"
               selectProps={{
@@ -189,24 +140,14 @@ export const ClassicPage: React.FC = () => {
                 components: { DropdownIndicator: () => null }
               }}
             />
-          </Box>
+          </BaseBox>
           {correctGuess && (
-            <Box
-              bg={'rgba(32, 70, 48, 1)'}
-              textColor={'white'}
-              p={4}
-              borderRadius="lg"
-              border={'0.25em solid lightgreen'}
-
-            >
-              <VStack>
-                <Text textAlign={'center'}>Correct! The character is {correctGuess}.</Text>
-                <Text textAlign={'center'} marginBottom={'4'}>It took you {incorrectGuesses.length + 1} attempts to
-                  guess correctly.</Text>
-                <PulsingButton label={'Next'} url={'/quote'} style={gotButtonStyle} />
-                <CountdownTimer />
-              </VStack>
-            </Box>
+            <ModeSuccessBox
+              correctGuess={correctGuess}
+              attempts={incorrectGuesses.length + 1}
+              label="Quote"
+              url="/quote"
+            />
           )}
           <CharacterGrid characterData={selectedCharacter} solutionCharacter={solutionCharacter} />
         </VStack>
