ed6faabdb48089bd97dd3b067454607b1ea83381
c0m0t3 <colin.moissl@icloud.com>
2025-01-07 11:52:05 +0100

    feat: add Basebox to Accordion

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -13,6 +13,7 @@ import {
 import { ChevronDownIcon } from '@chakra-ui/icons';
 import { useNavigate } from 'react-router-dom';
 import { gotButtonStyle } from '../styles/buttonStyles.ts';
+import { BaseBox } from '../components/BaseBox.tsx';
 
 export const HomePage = () => {
   const navigate = useNavigate();
@@ -53,48 +54,50 @@ export const HomePage = () => {
         <Button sx={gotButtonStyle} width="100%">Register</Button>
 
         <Container p={0}>
-          <Accordion allowMultiple>
-            <AccordionItem>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">What is GoTdle?</Box>
-                <ChevronDownIcon />
-              </AccordionButton>
-              <AccordionPanel>
-                <Text>
-                  GoTdle is a platform that offers a variety of Game of Thrones themed
-                  games and quizzes. It is designed to test your knowledge of the popular
-                  TV show and books, and provide entertainment for fans of the series.
-                </Text>
-              </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">Are there spoilers for the TV show?</Box>
-                <ChevronDownIcon />
-              </AccordionButton>
-              <AccordionPanel>
-                <Text>
-                  Yes, there may be spoilers for the TV show in some of the games and
-                  quizzes on GoTdle. If you have not watched all seasons of the show and
-                  want to avoid spoilers, we recommend catching up on the series before
-                  playing the games.
-                </Text>
-              </AccordionPanel>
-            </AccordionItem>
-            <AccordionItem>
-              <AccordionButton>
-                <Box flex="1" textAlign="left">How can I play the games?</Box>
-                <ChevronDownIcon />
-              </AccordionButton>
-              <AccordionPanel>
-                <Text>
-                  Just click on the "Play Now" button at the top of the page to get started.
-                  If you want to track your progress and compete with other players, you can create
-                  an account and log in to access additional features.
-                </Text>
-              </AccordionPanel>
-            </AccordionItem>
-          </Accordion>
+          <BaseBox width={'auto'}>
+            <Accordion allowMultiple>
+              <AccordionItem>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">What is GoTdle?</Box>
+                  <ChevronDownIcon />
+                </AccordionButton>
+                <AccordionPanel>
+                  <Text>
+                    GoTdle is a platform that offers a variety of Game of Thrones themed
+                    games and quizzes. It is designed to test your knowledge of the popular
+                    TV show and books, and provide entertainment for fans of the series.
+                  </Text>
+                </AccordionPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">Are there spoilers for the TV show?</Box>
+                  <ChevronDownIcon />
+                </AccordionButton>
+                <AccordionPanel>
+                  <Text>
+                    Yes, there may be spoilers for the TV show in some of the games and
+                    quizzes on GoTdle. If you have not watched all seasons of the show and
+                    want to avoid spoilers, we recommend catching up on the series before
+                    playing the games.
+                  </Text>
+                </AccordionPanel>
+              </AccordionItem>
+              <AccordionItem>
+                <AccordionButton>
+                  <Box flex="1" textAlign="left">How can I play the games?</Box>
+                  <ChevronDownIcon />
+                </AccordionButton>
+                <AccordionPanel>
+                  <Text>
+                    Just click on the "Play Now" button at the top of the page to get started.
+                    If you want to track your progress and compete with other players, you can create
+                    an account and log in to access additional features.
+                  </Text>
+                </AccordionPanel>
+              </AccordionItem>
+            </Accordion>
+          </BaseBox>
         </Container>
       </VStack>
     </Box>
