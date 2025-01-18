0433f08aacf36f529c79092999c9abc72a6625ea
Daniel <you@example.com>
2025-01-18 23:34:51 +0100

    chore: update cypress test

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -1,12 +1,31 @@
 describe('template spec', () => {
+  function generateRandomString(length) {
+    const characters =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length),
+      );
+    }
+    return result;
+  }
+
+  function generateRandomEmail() {
+    const randomString = generateRandomString(8);
+    return `${randomString}@example.com`;
+  }
+
   it('passes', () => {
-    cy.visit('/auth/login');
-    cy.url().should('contain', '/auth/login');
-    cy.get('#identifier').type('Bunte');
-    cy.findByLabelText(/password/i).type(
-      '$2b$10$eY8/c9ZmowANNdEheRd2C.MswcALaNcGMjKWR1nXYsoCoB./3xF5',
-    );
-    cy.findByText('Login User').click();
+    const randomUsername = generateRandomString(10);
+    const randomEmail = generateRandomEmail();
+
+    cy.visit('/auth/register');
+    cy.url().should('contain', '/auth/register');
+    cy.findByLabelText(/username/i).type(randomUsername);
+    cy.findByLabelText(/e-mail/i).type(randomEmail);
+    cy.findByLabelText(/password/i).type('Test123456');
+    cy.findByText('register').click();
 
     cy.url().should('contain', '/home');
     cy.findByText('Classic').click();
