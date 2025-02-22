f8e49fb061ec83b660339e2b59f49d4cead83f8c
sthiohnoo <hiroyuki.ohno@stud.h-da.de>
2025-02-22 15:38:13 +0100

    feat: add characterCards to display to all Characters

--- frontend/src/pages/AdminPage.tsx
+++ frontend/src/pages/AdminPage.tsx
@@ -1,4 +1,5 @@
 import {
+  Box,
   Tab,
   TabList,
   TabPanel,
@@ -11,35 +12,43 @@ import { BaseLayout } from '../layout/BaseLayout.tsx';
 import { BaseBox } from '../components/BaseBox.tsx';
 import { ManageCharacter } from './adminPageComponents/ManageCharacter.tsx';
 import { ManageUser } from './adminPageComponents/ManageUser.tsx';
+import { useState } from 'react';
+import { CharacterCard } from './adminPageComponents/CharacterCard.tsx';
 
 export const AdminPage = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
-    <BaseLayout>
-      <VStack spacing={4} align="stretch" my={4}>
-        <BaseBox width={'40em'}>
-          <Text textAlign={'center'} fontSize={'2em'}>
-            Admin Dashboard
-          </Text>
-        </BaseBox>
+    <>
+      <BaseLayout>
+        <Box width={'100%'} maxWidth={'none'}>
+          <Tabs onChange={(index) => setActiveTab(index)}>
+            <VStack spacing={4} my={4}>
+              <BaseBox width={'40em'}>
+                <Text textAlign={'center'} fontSize={'2em'}>
+                  Admin Dashboard
+                </Text>
+                <TabList mt={2}>
+                  <Tab>User List</Tab>
+                  <Tab>Manage Characters</Tab>
+                </TabList>
+              </BaseBox>
 
-        <BaseBox width={'40em'}>
-          <Tabs>
-            <TabList my={2}>
-              <Tab>User List</Tab>
-              <Tab>Manage Characters</Tab>
-            </TabList>
-
-            <TabPanels>
-              <TabPanel>
-                <ManageUser />
-              </TabPanel>
-              <TabPanel>
-                <ManageCharacter />
-              </TabPanel>
-            </TabPanels>
+              <BaseBox width={'40em'}>
+                <TabPanels>
+                  <TabPanel>
+                    <ManageUser />
+                  </TabPanel>
+                  <TabPanel>
+                    <ManageCharacter />
+                  </TabPanel>
+                </TabPanels>
+              </BaseBox>
+              {activeTab === 1 && <CharacterCard />}
+            </VStack>
           </Tabs>
-        </BaseBox>
-      </VStack>
-    </BaseLayout>
+        </Box>
+      </BaseLayout>
+    </>
   );
 };
